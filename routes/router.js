651efcc3793a1b3769362e285bea421db834bd4f
Rob Smith <rsmith-63@users.noreply.github.com>
2017-01-24 17:00:16 -0500

    Add files via upload

---
+++ routes/router.js
@@ -0,0 +1,20 @@
+/**
+ * Created by rob on 1/24/2017.
+ */
+"use strict";
+
+const router = require('koa-router')(), fs = require("fs"), path = require("path"), _ = require("lodash");
+//Set up router
+
+
+
+//Add routes to handle our API requests so far, using fake data
+router.get("/pubMed", function *(ctx){
+    this.body = { test:"test only"};
+    this.type = "application/json";
+
+});
+
+
+
+module.exports = router;
